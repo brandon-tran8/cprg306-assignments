9abca9973707c955ba148d2f7a92c65e4ccae79a
Brandon Tran <brandonwtran@gmail.com>
2024-01-27 00:49:14 -0700

    added GitHub link

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,7 +4,7 @@ export default function StudentInfo() {
       <main>
         <h1>Brandon Tran</h1>
         <ul>
-          <li><Link href="">My GitHub</Link></li>
+          <li><Link href="https://github.com/brandon-tran8/cprg306-assignments.git">My GitHub</Link></li>
         </ul>
       </main>
     );
