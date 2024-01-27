24bffb752a44a281eb582ccfd9525b00b7b09d08
Brandon Tran <brandonwtran@gmail.com>
2024-01-27 00:45:49 -0700

    webpage created (week2)

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+export default function StudentInfo() {
+    return (
+      <main>
+        <h1>Brandon Tran</h1>
+        <ul>
+          <li><Link href="">My GitHub</Link></li>
+        </ul>
+      </main>
+    );
+  }
